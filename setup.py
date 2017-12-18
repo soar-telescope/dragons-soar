38418404ac54c18ba287f5aa898b5b678236dea6
Bruno Quint <bquint@ctio.noao.edu>
2017-12-18 03:09:36 -0300

    Updated .travis.yml

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version='0.0.1',
 
     description='Data-Reduction Pipeline Suite for the SOAR Telescope using '
                 'the DRAGONS Framework.',
