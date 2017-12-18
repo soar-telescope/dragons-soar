e923aee82fb036466a869596507f2a7008bb5e5c
Bruno Quint <bquint@ctio.noao.edu>
2017-12-18 01:54:05 -0300

    Added reinstall, updated travis and setup

---
+++ tests/test_sami.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import pkg_resources
+
+import astrodata
+import soar_instruments
+
+
+class TestSAMI(unittest.TestCase):
+
+    def test_bias(self):
+
+        sample_bias = pkg_resources.resource_filename(
+            'soar_instruments', 'sample_data/sami_bias.fits')
+
+        ad = astrodata.open(sample_bias)
+        assert isinstance(1, int)
+
+
+
