1d96d6af5b195c43842a801b3fa48ca76579d935
Bruno Quint <bquint@ctio.noao.edu>
2017-12-18 22:31:17 -0300

    Updated test_sami

--- tests/test_sami.py
+++ tests/test_sami.py
@@ -2,16 +2,25 @@
 
 import unittest
 import pkg_resources
+import os
 
 import soar_instruments
 import astrodata
 
 
-#class TestSAMI(unittest.TestCase):
-#
-#        sample_bias = pkg_resources.resource_filename(
-#            'soar_instruments', 'sample_data/sami_bias.fits')
-#
-#       ad = astrodata.open(sample_bias)
-#
+class TestSAMI(unittest.TestCase):
+
+    def file_exists(self):
+
+        sample_bias = pkg_resources.resource_filename(
+            'soar_instruments', 'sample_data/sami_bias.fits')
+
+        assert os.path.exists(sample_bias)
+
+    def test_bias(self):
+
+        sample_bias = pkg_resources.resource_filename(
+            'soar_instruments', 'sample_data/sami_bias.fits')
+
+        ad = astrodata.open(sample_bias)
 
