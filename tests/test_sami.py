35c93d406164feeead5cff2b5844f18f9440d6ec
Bruno Quint <bquint@ctio.noao.edu>
2017-12-19 00:15:24 -0300

    Updated test_sami - removed EXTNAME from extensions

--- tests/test_sami.py
+++ tests/test_sami.py
@@ -4,6 +4,8 @@ import unittest
 import pkg_resources
 import os
 
+from astropy.io import fits as pyfits
+
 import soar_instruments
 import astrodata
 
@@ -19,6 +21,14 @@ class TestSAMI(unittest.TestCase):
 
         # Check if file exists
         assert os.path.exists(sample_bias)
+
+        # ToDo - Temporary fix for the issue regarding the EXTNAME
+        hdu = pyfits.open(sample_bias)
+        for i in range(1, len(hdu)):
+            del hdu[i].header['EXTNAME']
+
+        sample_bias = sample_bias.replace('.fits', '_c.fits')
+        hdu.writeto(sample_bias, overwrite=True)
 
         # Try to open in astrodata
         ad = astrodata.open(sample_bias)
