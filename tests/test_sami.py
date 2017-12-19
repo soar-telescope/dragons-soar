18838d0abe393a988a7cc57c6b0e8638bce0d471
Bruno Quint <bquint@ctio.noao.edu>
2017-12-18 23:21:28 -0300

    Updated test_sami - only reading the file

--- tests/test_sami.py
+++ tests/test_sami.py
@@ -10,17 +10,17 @@ import astrodata
 
 class TestSAMI(unittest.TestCase):
 
-    def file_exists(self):
+    def test_bias(self):
+        """
+        Just check if the a BIAS obtained with SAMI is read correctly and if
+        the tags are set properly.
+        """
+        sample_bias = 'tests/data/sami/bias.fits'
 
-        sample_bias = pkg_resources.resource_filename(
-            'soar_instruments', 'sample_data/sami_bias.fits')
-
+        # Check if file exists
         assert os.path.exists(sample_bias)
 
-#    def test_bias(self):#
-#
-#        sample_bias = pkg_resources.resource_filename(
-#            'soar_instruments', 'sample_data/sami_bias.fits')#
-#
-#        ad = astrodata.open(sample_bias)
+        # Try to open in astrodata
+        ad = astrodata.open(sample_bias)
 
+        print ad.info()
